fc37903d3a0cdaa017d8bf65d3162637af1d33b5
Baldi00 <andrea.baldelli2000@gmail.com>
2023-06-27 20:11:31 +0200

    Generalized map generation into a static method

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,35 @@
+using DBGA.MapGeneration;
+using DBGA.Tiles;
+using UnityEngine;
+
+namespace DBGA.GameManager
+{
+    [DisallowMultipleComponent]
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Map generation")]
+        [SerializeField]
+        private bool generateRandomMap;
+        [SerializeField]
+        private int gridSize;
+        [SerializeField]
+        private TilesList tilesList;
+
+        private Tile[][] grid;
+
+        void Awake()
+        {
+            if (generateRandomMap)
+            {
+                grid = MapGenerator.GenerateMap(gridSize, tilesList);
+                for (int row = 0; row < gridSize; row++)
+                    for (int col = 0; col < gridSize; col++)
+                        Instantiate(
+                            grid[row][col].gameObject,
+                            new Vector3(row, 0f, col),
+                            Quaternion.identity,
+                            transform);
+            }
+        }
+    }
+}
--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace DBGA.MapGeneration
 {
     [DisallowMultipleComponent]
-    public class MapGenerator : MonoBehaviour
+    public class MapGenerator
     {
         public struct ProbabilityRange
         {
@@ -13,42 +13,32 @@ namespace DBGA.MapGeneration
             public float max;
         }
 
-        [SerializeField]
-        private int gridSize;
-        [SerializeField]
-        private TilesList tileList;
-
-        private Tile[][] grid;
-
-        private Dictionary<ProbabilityRange, Tile> availableTilesWithProbability;
-
-        void Awake()
+        public static Tile[][] GenerateMap(int gridSize, TilesList tileList)
         {
-            InitializeTilesAndProbability();
-        }
+            Dictionary<ProbabilityRange, Tile> availableTilesWithProbability = InitializeTilesAndProbability(tileList);
 
-        void Start()
-        {
-            grid = new Tile[gridSize][];
+            Tile[][] grid = new Tile[gridSize][];
+
             for (int row = 0; row < gridSize; row++)
             {
                 grid[row] = new Tile[gridSize];
                 for(int col = 0; col < gridSize; col++)
                 {
-                    grid[row][col] = GetRandomTileFromAvailable();
+                    grid[row][col] = GetRandomTileFromAvailable(availableTilesWithProbability);
                     grid[row][col].SetPosition(row, col);
-                    Instantiate(grid[row][col].gameObject, new Vector3(row, 0f, col), Quaternion.identity, transform);
                 }
             }
+
+            return grid;
         }
 
-        private void InitializeTilesAndProbability()
+        private static Dictionary<ProbabilityRange, Tile> InitializeTilesAndProbability(TilesList tileList)
         {
             float totalTilesProbabilityWeight = 0;
             foreach(TileListItem tileListItem in tileList.availableTiles)
                 totalTilesProbabilityWeight += tileListItem.probabilityWeight;
 
-            availableTilesWithProbability = new Dictionary<ProbabilityRange, Tile>();
+            Dictionary<ProbabilityRange, Tile> availableTilesWithProbability = new Dictionary<ProbabilityRange, Tile>();
             float previousRangeMin = 0f;
             foreach(TileListItem tileListItem in tileList.availableTiles)
             {
@@ -58,9 +48,11 @@ namespace DBGA.MapGeneration
 
                 previousRangeMin = rangeMax;
             }
+
+            return availableTilesWithProbability;
         }
 
-        private Tile GetRandomTileFromAvailable()
+        private static Tile GetRandomTileFromAvailable(Dictionary<ProbabilityRange, Tile> availableTilesWithProbability)
         {
             float randomNumber = Random.Range(0f, 1f);
             foreach(KeyValuePair<ProbabilityRange, Tile> tile in availableTilesWithProbability)
